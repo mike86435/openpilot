484b25b941901eab30cb4d8f658bff187e71e367
mike8643 <98910897+mike8643@users.noreply.github.com>
2023-02-26 17:13:20 -0500

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -167,10 +167,10 @@ function two_init {
     tar -zxvf "$LIB_PATH/$MODULE.tar.gz" -C "$PY_LIB_DEST/"
   fi
   # panda
-  if [ ! -f "$PY_LIB_DEST/spidev.cpython-38.so" ]; then
-    echo "Installing spidev.cpython-38.so..."
-    cp -f "$LIB_PATH/spidev.cpython-38.so" "$PY_LIB_DEST/"
-  fi
+#  if [ ! -f "$PY_LIB_DEST/spidev.cpython-38.so" ]; then
+#    echo "Installing spidev.cpython-38.so..."
+#    cp -f "$LIB_PATH/spidev.cpython-38.so" "$PY_LIB_DEST/"
+#  fi
   mount -o remount,r /system
 
   # Check for NEOS update
